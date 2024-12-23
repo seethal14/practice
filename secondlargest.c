f5de8f279ce33e7644ec100669f2ad4ebabb8ba8
seethal14 <seethalsaseendran14@gmail.com>
2024-12-23 14:06:25 +0530

    Update and rename secondlargest to secondlargest.c

--- secondlargest.c
+++ secondlargest.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main(){
     int arr[100],temp,i,j,n;
-    printf("enter the  size");
+    printf("enter the  size\n");
     scanf("%d",&n);
-    printf("enter the elements");
+    printf("enter the elements\n");
     for(i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
     for(i=0;i<=n-2;i++){
-        for(j=1;j<=n-1;j++){
+        for(j=i+1;j<=n-1;j++){
             if(arr[i]>arr[j]){
                 temp=arr[i];
                 arr[i]=arr[j];
@@ -16,9 +16,12 @@ int main(){
             }
         }
     }
-    
+    printf("sorted array\n");
     for(i=0;i<n;i++){
-        printf("%d",arr[i]);
 
-}
+        printf(" %d\n",arr[i]);
+    }
+    printf("secondlargest number\n %d",arr[1]);
+
+
 }
