664043fad317cf7df55aba2c681939486ac53aa8
seethal14 <seethalsaseendran14@gmail.com>
2024-12-23 11:16:09 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int ar[100],temp,i,max,n;
+    printf("enter the size");
+    scanf("%d",&n);
+    printf("enter the elements");
+    for(i=0;i<n;i++){
+        scanf("%d",&ar[i]);
+    }
+    max=ar[0];
+    for(i=0;i<=n;i++){
+        if(ar[i]>max){
+            temp=ar[i];
+            ar[i]=max;
+            max=temp;
+        }
+    }
+    printf("tha largest number %d",max);
+
+
+}
