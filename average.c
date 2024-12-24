f3d2e0fd4ed9ddd65b2b133439a49ff4cb552eda
seethal14 <seethalsaseendran14@gmail.com>
2024-12-24 10:29:06 +0530

    Create average.c

---
+++ average.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int arr[100],sum=0,count=0,i,n,average;
+    printf("enter the  size\n");
+    scanf("%d",&n);
+    printf("enter the elements\n");
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+
+    }
+    for(i=0;i<n;i++){
+        sum=sum+arr[i];
+        count++;
+    }
+    printf("sum of elements %d\n",sum);
+    printf("number of elements %d\n",count);
+    average=sum/count;
+    printf("average %d\n",average);
+    
+}
