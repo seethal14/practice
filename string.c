f107767a6074f5b511da35309e1073fc792d50f6
seethal14 <seethalsaseendran14@gmail.com>
2024-12-24 12:50:03 +0530

    Update string.c

--- string.c
+++ string.c
@@ -5,9 +5,12 @@ int main(){
    char s1[100]="hello world";
    printf("string is %s\n",s1);
     int length=strlen(s1);
+    printf("%d\n",length);
     printf("reversed string\n");
-    for(int i=length;i>0;i--){
+    
+    for(int i=length-1;i>=0;i--){
         printf("%s",s1[i]);
+        
     }
     
 
