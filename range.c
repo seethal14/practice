823324f43713899f8f389653874783ff8c500a04
seethal14 <seethalsaseendran14@gmail.com>
2024-12-24 23:28:53 +0530

    Update range.c

--- range.c
+++ range.c
@@ -1,17 +1,20 @@
 #include<stdio.h>
 int main(){
     int a=0;
-    int arr[100][100]={{1,5},{4,10}};
+    int arr[100][100]={{5,10},{1,6}};
     for(int i=arr[0][0];i<=arr[0][1];i++){
-        printf("%d\n",i);
-        if(i==arr[1][0]){
-            a=1;
-
+       
+        
+     for(int j=arr[1][0];j<=arr[1][1];j++){
+           
+            if(i==j){
+                a=1;
+            }
         }
        
         
     }
-    if(a==1){
+    if(a=1){
         printf("overlaped\n");
     }
     else{
