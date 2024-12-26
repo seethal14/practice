524f2575e96c857d2323d109a481116441836356
seethal14 <seethalsaseendran14@gmail.com>
2024-12-27 00:58:06 +0530

    Update stringfound.c

--- stringfound.c
+++ stringfound.c
@@ -1,14 +1,28 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-    char s1[100],s2[100];
+    char s1[100],s2[100],s3[100];
     printf("eneter a string\n");
     scanf("%s",&s1);
     printf("enter find out string\n");
     scanf("%s",&s2);
-    if(strstr(s1,s2)!=NULL){
-        printf("string is found");
+    int len1=strlen(s1);
+    int len2=strlen(s2);
+    for(int i=0;i<len2;i++){
+        for(int j=0;j<len1;j++){
+            if(s2[i]==s1[j]){
+                s3[i]=s1[j];
+                i++;
+
+            }
+        }
+    }
+    
+
+    if(s3==s2){
+        printf("found");
     }
     else{
         printf("not found");
     }
+}
