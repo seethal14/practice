30d0e89f57e12cf15889b9d2d0b06a72f64d7e94
seethal14 <seethalsaseendran14@gmail.com>
2024-12-30 21:43:05 +0530

    Create countofcharacter.c

---
+++ countofcharacter.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char s1[100],c;
+    printf("enter string\n");
+    scanf("%s",&s1);
+    printf("enter the character\n");
+    scanf("%c",&c);
+    int count=0;
+    int length=strlen(s1);
+    for(int i=0;i<length;i++){
+        if(s1[i]==c){
+            count++;
+
+        }
+    }
+    printf("count of character %d\n",count);
+
+}
