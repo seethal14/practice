25baf3137e8d7ed307740df23871af9ce54cfcbd
seethal14 <seethalsaseendran14@gmail.com>
2025-01-05 19:19:49 +0530

    Create charchange2.c

---
+++ charchange2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char s1[100],c1,c2;
+    printf("enter a string\n");
+    scanf("%s",&s1);
+    printf("enter changed character");
+    scanf("%c",&c1);
+    printf("enter new character");
+    scanf("%c",&c2);
+    int length=strlen(s1);
+    for(int i=0;i<length;i++){
+        if(s1[i]==c1){
+            s1[i]=c2;
+        }
+    }
+    printf("changed string");
+    for(int i=0;i<length;i++){
+        printf(" %c",s1[i]);
+    }
+}
