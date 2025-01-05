1fd38b64ac4543a0b318041a40bb4576ae401ab9
seethal14 <seethalsaseendran14@gmail.com>
2025-01-05 19:33:22 +0530

    Update charchange2.c

--- charchange2.c
+++ charchange2.c
@@ -3,19 +3,21 @@
 int main(){
     char s1[100],c1,c2;
     printf("enter a string\n");
-    scanf("%s",&s1);
-    printf("enter changed character");
-    scanf("%c",&c1);
-    printf("enter new character");
-    scanf("%c",&c2);
+    scanf("%s", &s1);
+
+    printf("enter changed character\n");
+    scanf(" %c",&c1);
+
+    printf("enter new character\n");
+    scanf(" %c",&c2);
     int length=strlen(s1);
     for(int i=0;i<length;i++){
         if(s1[i]==c1){
             s1[i]=c2;
         }
     }
-    printf("changed string");
+    printf("changed string\n");
     for(int i=0;i<length;i++){
-        printf(" %c",s1[i]);
+        printf("%c",s1[i]);
     }
 }
